213ba0c09af118625f4b9b86b3c7f12d2697df10
stillaalive <35803844+stillaalive@users.noreply.github.com>
2018-02-05 00:22:20 +0300

    Add files via upload

---
+++ d3.py
@@ -0,0 +1,27 @@
+# print("Факториалы чисел")
+# b=float(1)
+# c=float(1)
+# a=float(input("Введите число: "))
+# while b<a:
+#   b+=1
+#   c=c*b
+# print("Факториал числа ",a," равен = ",c)
+
+print("Факториалы чисел")
+b=float(1)
+c=float(1)
+d=float
+while True:
+    a=float(input("Введите число: "))
+    while b<a:
+        b+=1
+        c=c*b
+        d=c
+        if d==c:
+            continue
+    print("Факториал числа ",a," равен = ",c)
+       
+
+    
+
+
